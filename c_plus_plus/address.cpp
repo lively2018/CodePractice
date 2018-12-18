ae1655096a4f25372061a0acb6ba0821c173fbd3
Kyungsoon Song <kaebbi@gmail.com>
2018-12-18 12:48:02 +0900

    Add defined data type

---
+++ c_plus_plus/address.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int donuts = 6;
+	double cups = 4.5;
+
+	cout << "donuts value = " << donuts;
+	cout << " and donuts address = " << &donuts << "\n";
+	cout << "cups value = " <<  cups;
+	cout << " and cups address = " << &cups << "\n";
+	return 0;
+}
